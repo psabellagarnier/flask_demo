f3cca7975b81966ec7ece40c136bf73827fc2632
Philippe Sabella-Garnier <philippe.sg@gmail.com>
2020-09-14 15:58:23 -0700

    added interaction

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+entered_text = ''
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('form.html', outside_title='This is a title!')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+@app.route('/form_action', methods=['GET', 'POST'])
+def form_action():
+  entered_text = request.form['form_input']
+  return render_template('graph.html', input_text=entered_text)
 
 if __name__ == '__main__':
   app.run(port=33507)
